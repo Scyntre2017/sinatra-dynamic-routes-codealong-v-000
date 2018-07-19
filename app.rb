2aa27182dd827876705bbaa698cb40e865467bf2
Christopher Jones <cjones999983@gmail.com>
2018-07-19 02:46:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require "pry"
 
 class App < Sinatra::Base
 
-  # This is a sample static s.
+  # This is a sample static route.
   get '/hello' do
     "Hello World!"
   end
 
-  # This is a sample dynamic s.
+  # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
